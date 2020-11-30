263fdb2457d028b1d7399bdec1ca5011f11a8e7b
Lautaro <Lautaro@LAUTARO-PC>
2020-11-30 16:08:47 -0300

    ...

--- src/ar/edu/unq/po2/tp6/Banco/Banco.java
+++ src/ar/edu/unq/po2/tp6/Banco/Banco.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 
 public class Banco {
 	private ArrayList<Cliente> clientes;
+	private Float monto; 
 	
 	public Banco() {
 		this.clientes = new ArrayList<Cliente>();
+		this.monto = 0f;
 	}
 	
 	public void agregarCliente(Cliente cliente) {
@@ -20,5 +22,10 @@ public class Banco {
 	}
 
 	private void otorgarSolicitud(SolicitudDeCredito solicitud) {
+		this.monto += solicitud.monto;
+	}
+	
+	public Float getMontoADesembolsar() {
+		return this.monto;
 	}
 }
